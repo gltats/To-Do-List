b55ba5aeb4cfafc477fb016b704e3a1f29850669
gltats <74324703+gltats@users.noreply.github.com>
2021-04-01 20:44:22 +0200

    Currently page information

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import logo from './logo.svg';
 import './App.css';
+import Loader from "react-loader-spinner";
+import "bootstrap/dist/css/bootstrap.css"
 
 function sayHello(){
   console.log("Hello")
 }
 
 const Header = () => {
-  return <h1>Click the button!</h1>;
+  return <h3>Click the button!</h3>;
 }
 
 class Button extends React.Component{
@@ -23,8 +25,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Hi!
+          Hi sweetty!🥰
         </h1>
+        <p>
+          This page is under development, please come back later
+          <Loader
+      className="loader"
+        type="Watch"
+        color="#00BFFF"
+        height={100}
+        width={100} 
+      />
+        </p>
+        <hr className="separator"/>
         <Header />
         <Button />
       </header>
